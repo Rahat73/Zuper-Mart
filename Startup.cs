1b9a126261a120e1a98caf88edac36249ef9f2e6
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-03-06 00:56:29 +0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zuper_Mart.Startup))]
+namespace Zuper_Mart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
